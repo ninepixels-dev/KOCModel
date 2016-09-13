5844fe56e3b98577b29abe26bbd1e50a0acf835b
pavlovicnemanja <me@pavlovicnemanja.com>
2016-09-13 17:01:43 +0200

    feat(h2s): Improved functionality of H2S

--- public_html/assets/scripts/concept-selection/concept-distances/h2s-facilities/concept-stage-h2s-facilities.js
+++ public_html/assets/scripts/concept-selection/concept-distances/h2s-facilities/concept-stage-h2s-facilities.js
@@ -47,21 +47,128 @@ function h2sFacilities() {
                 return '<line class="' + axis + '" value="' + value + '" title="Value: ' + value + '"></line>';
             }
 
+            $('input[name="h2s-value"]').change(function () {
+                var val = $(this).val();
+
+                if (val > 0 && val < 1) {
+                    return true;
+                }
+
+                $(this).val('');
+            });
+
             $('form').submit(function (e) {
                 e.preventDefault();
                 var h2sValue = $('input[name="h2s-value"]').val();
                 var pValue = $('input[name="p-value"]').val();
-
                 var result = h2sValue * Math.sqrt(pValue);
+                var rounded = Math.round(result * 10) / 10;
+
+                var parameters = {
+                    a: {
+                        650: 230.73,
+                        300: 420.06,
+                        100: 659.51,
+                        30: 941.6
+                    },
+                    b: {
+                        650: 1.1329,
+                        300: 0.92,
+                        100: 0.76,
+                        30: 0.6297
+                    }
+                };
 
                 $('.y').removeClass('active');
-                $('.y[value="' + result + '"]').addClass('active');
+                $('.y[value="' + rounded + '"]').addClass('active');
 
-                $('.result').html(result);
+                $('.result').html(rounded);
+
+                angular.forEach(parameters.a, function (value, index) {
+                    var partResult = parameters.a[index] * Math.pow(rounded, parameters.b[index]);
+                    $('.result--' + index).html(Math.round(partResult));
+                });
+
+                scope.$apply(function () {
+                    scope.additional = true;
+                });
             });
         }
     };
 }
 
+blowouts50.$inject = [];
+function blowouts50() {
+    return {
+        link: function (scope) {
+            $('form').submit(function (e) {
+                var h2sValue = $('input[name="h2s-value"]').val();
+
+                var parameters = {
+                    a: {
+                        650: 2200,
+                        300: 2200,
+                        100: 3500,
+                        30: 6000
+                    },
+                    b: {
+                        650: 0.95,
+                        300: 0.95,
+                        100: 0.75,
+                        30: 0.62
+                    }
+                };
+
+                angular.forEach(parameters.a, function (value, index) {
+                    var partResult = parameters.a[index] * Math.pow((h2sValue / 100), parameters.b[index]);
+                    $('.result--' + index).html(Math.round(partResult));
+                });
+
+                scope.$apply(function () {
+                    scope.additional = true;
+                });
+            });
+        }
+    };
+}
+
+blowouts100.$inject = [];
+function blowouts100() {
+    return {
+        link: function (scope) {
+            $('form').submit(function (e) {
+                var h2sValue = $('input[name="h2s-value"]').val();
+
+                var parameters = {
+                    a: {
+                        650: 2200,
+                        300: 2200,
+                        100: 3500,
+                        30: 6000
+                    },
+                    b: {
+                        650: 0.95,
+                        300: 0.95,
+                        100: 0.75,
+                        30: 0.62
+                    }
+                };
+
+                angular.forEach(parameters.a, function (value, index) {
+                    var partResult = parameters.a[index] * Math.pow((h2sValue / 100), parameters.b[index]);
+                    $('.result--' + index).html(Math.round(partResult));
+                });
+
+                scope.$apply(function () {
+                    scope.additional = true;
+                });
+            });
+        }
+    };
+}
+
+
 angular.module('fieldSpacingTool.h2sFacilities', [])
-        .directive('h2sFacilities', h2sFacilities);
+        .directive('h2sFacilities', h2sFacilities)
+        .directive('blowouts50', blowouts50)
+        .directive('blowouts100', blowouts100);
